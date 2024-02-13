07dba6b5ae1ac287b0f95b99542816e6b77a2c82
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-02-13 17:30:57 +0100

    function component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Greet from './components/Greet'
 
 function App() {
   return (
     <div className="App">
-      <h1>Tutorial</h1>
+      <Greet />
     </div>
   );
 }
---
+++ src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// function Greet() {
+//     return <h1>Hello Developer</h1>
+// }
+
+// function component w. ES6 syntax
+// As arrow function return Value by default
+const Greet = () => <h1>Hello Developer</h1>
+
+export default Greet
