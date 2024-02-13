59b67bb710ff591bb1a822152205ba1457768aac
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-02-13 18:14:38 +0100

    class component

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -6,6 +6,6 @@ import React from 'react'
 
 // function component w. ES6 syntax
 // As arrow function return Value by default
-const Greet = () => <h1>Hello Developer</h1>
+const Greet = () => <h1>Hello Developer from react function component</h1>
 
 export default Greet
