9a7f5629d1af00e56c1917f38a821f5580a9a270
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-02-13 21:20:00 +0100

    react component w. JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
+import JSXGreet from './components/JSXGreet';
 
 function App() {
   return (
     <div className="App">
       <Greet />
       <Welcome />
+      <JSXGreet />
     </div>
   );
 }
---
+++ src/components/JSXGreet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const JSXGreet = () => {
+    return (
+        <div>
+            <h1>Hello Developer from react JSX function component</h1>
+        </div>
+    )
+}
+
+export default JSXGreet
