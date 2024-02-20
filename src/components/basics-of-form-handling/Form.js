c95300eb06e5249511f2ca3e0819c8fd575718f5
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-02-20 16:05:57 +0100

    react forms example completed

--- src/components/basics-of-form-handling/Form.js
+++ src/components/basics-of-form-handling/Form.js
@@ -7,11 +7,13 @@ class Form extends Component {
         super(props)
 
         this.state = {
-            username: ''
+            username: '',
+            comments: '',
+            topic: 'react'
         }
     }
     // handler method as a class property
-    // and is to be equal an arrow function
+    // and is will to be equal to the arrow function
     handleUsernameChange = (event) => {
         this.setState(
             {
@@ -23,9 +25,36 @@ class Form extends Component {
             })
     }
 
+    handleCommentsChange = (event) => {
+        this.setState(
+            {
+                comments: event.target.value
+            },
+            // callback
+            () => {
+                console.log(this.state.comments)
+            })
+    }
+
+    handleTopicChange = (event) => {
+        this.setState(
+            {
+                topic: event.target.value
+            },
+            // callback
+            () => {
+                console.log(this.state.topic)
+            })
+    }
+
+    handelSubmit = event => {
+       alert(`${this.state.username} ${this.state.comments} ${this.state.topic}`)
+       event.preventDefault()
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handelSubmit}>
                 <div>
                     <label>Username</label>
                     <input
@@ -34,6 +63,24 @@ class Form extends Component {
                         onChange={this.handleUsernameChange}
                     />
                 </div>
+                <div>
+                    <label>Comments</label>
+                    <textarea
+                        value={this.state.comments}
+                        onChange={this.handleCommentsChange}
+                    />
+                </div>
+                <div>
+                    <label>Topic</label>
+                    <select
+                        value={this.state.topic}
+                        onChange={this.handleTopicChange}>
+                        <option value="react">React</option>
+                        <option value="angular">Angular</option>
+                        <option value="vue">Vue</option>
+                    </select>
+                </div>
+                <button type="submit">Submit</button>
             </form>
         )
     }
