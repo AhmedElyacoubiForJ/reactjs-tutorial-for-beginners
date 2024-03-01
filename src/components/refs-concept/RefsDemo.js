85ee4b21fe86bf6975e578c5dda93f0d0763b4b3
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-01 12:52:10 +0100

    callback ref approach

--- src/components/refs-concept/RefsDemo.js
+++ src/components/refs-concept/RefsDemo.js
@@ -5,11 +5,20 @@ export default class RefsDemo extends Component {
     // rconst
     constructor(props) {
         super(props)
+        // first approch to make refs on elements
         this.inputRef = React.createRef()
+        // second approach to make refs on elements using callback methods
+        this.callBackRef = null
+        this.setCallBackRef = element  => {
+            this.callBackRef = element
+        }
     }
     componentDidMount() {
-        this.inputRef.current.focus()
-        console.log(this.inputRef)
+        /* this.inputRef.current.focus()
+        console.log(this.inputRef) */
+        if (this.callBackRef) {
+            this.callBackRef.focus()
+        }
     }
     clickHandler = () => {
         alert(this.inputRef.current.value)
@@ -18,6 +27,7 @@ export default class RefsDemo extends Component {
         return (
             <div>
                 <input type="text" ref ={this.inputRef}/>
+                <input type="text" ref ={this.setCallBackRef}/>
                 <button onClick={this.clickHandler}>Click</button>
             </div>
         )
